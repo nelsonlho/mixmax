afd7f2247565e711ed8e032b34aa89434d3d27fa
nelsonlho <nelsonlho@gmail.com>
2016-08-01 01:10:56 -0700

    added function to get dimensions of poster

--- api/resolver.js
+++ api/resolver.js
@@ -1,41 +1,42 @@
 var key = require('../utils/key');
 var sync = require('synchronize');
 var request = require('request');
-var sizeOf = require('image-size');
+var size = require('request-image-size');
+var parseURL = require('url');
 var _ = require('underscore');
 
 
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
   var url = req.query.url.trim();
+  var parsedURL = parseURL.parse(url);
 
-  // Giphy image urls are in the format:
-  // http://giphy.com/gifs/<seo-text>-<alphanumeric id>
-  var matches = url.match(/\-([a-zA-Z0-9]+)$/);
-  if (!matches) {
+  // Getting the query portion of the URL
+  // Movies are to be searched in the format http://www.omdbapi.com/?t=titanic
+  var query = parsedURL.query;
+  if (!query) {
     res.status(400).send('Invalid URL format');
     return;
   }
 
-  var id = matches[1];
-
   var response;
   try {
     response = sync.await(request({
-      url: 'http://www.omdbapi.com/?t=' + encodeURIComponent(id),
-      gzip: true,
-      json: true,
-      timeout: 15 * 1000
+      url: 'http://www.omdbapi.com/?' + query,
+      timeout: 15*1000
     }, sync.defer()));
   } catch (e) {
     res.status(500).send('Error');
     return;
   }
+  var movieObject = JSON.parse(response.body);
 
-  var image = response.Poster;
-  var dimensions = sizeOf(image);
-  var width = dimensions.width > 600 ? 600 : dimensions.width;
-  var html = '<img style="max-width:100%;" src="' + image.url + '" width="' + width + '"/>';
+  var image = movieObject.Poster;
+  var width;
+  size(image, function(err, dimensions, length){
+    width = dimensions.width;
+  });
+  var html = '<img style="max-width:100%;" src="' + image + '" width="' + width + '"/>';
   res.json({
     body: html
     // Add raw:true if you're returning content that you want the user to be able to edit
